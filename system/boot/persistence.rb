526d5d25b7b8d73a5bd46bd8c611ae7c9a7188a4
Peter Berkenbosch <info@peterberkenbosch.nl>
2020-02-20 15:23:44 +0100

    ROM container needs a sql connection
    The ROM Container needs a connection, since we are using
    a ROM::Configuration in `db.config` we have to pass this in
    here too and use the default gateway connection.

--- system/boot/persistence.rb
+++ system/boot/persistence.rb
@@ -1,6 +1,6 @@
 Bix::Application.boot(:persistence) do |app|
   start do
-    container = ROM.container(:sql, app['db.connection']) do |config|
+    container = ROM.container(:sql, app['db.config'].gateways[:default].connection) do |config|
       config.auto_registration(app.root + "lib/bix")
     end
     register('container', container)
